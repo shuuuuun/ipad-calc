3b43d89bfe35a98e18c7807b922a74ee7c77c8d7
motoki-shun <motoki-shun@kayac.com>
2018-07-21 23:04:59 +0900

    BackgroundHighlightedButton
    ref. https://qiita.com/wasnot/items/3537d932e9550a5c4c27

---
+++ calculator/BackgroundHighlightedButton.swift
@@ -0,0 +1,25 @@
+//
+//  BackgroundHighlightedButton.swift
+//  calculator
+//
+//  Created by motoki-shun on 2018/07/20.
+//  Copyright © 2018年 motoki-shun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BackgroundHighlightedButton: UIButton {
+    @IBInspectable var highlightedBackgroundColor :UIColor?
+    @IBInspectable var nonHighlightedBackgroundColor :UIColor?
+    override var isHighlighted :Bool {
+        didSet {
+            if isHighlighted {
+                self.backgroundColor = highlightedBackgroundColor
+            }
+            else {
+                self.backgroundColor = nonHighlightedBackgroundColor
+            }
+        }
+    }
+}
